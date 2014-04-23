5d7704b9a87295aab4e9be343a913e08e576b0a0
Hrishabh Gupta <hrishabh@zemosolabs.com>
2014-04-23 19:16:09 +0530

    parse function updated

--- lib/libphonenumber-execjs/libphonenumber.rb
+++ lib/libphonenumber-execjs/libphonenumber.rb
@@ -15,7 +15,36 @@ class Libphonenumber
   end
   
   def parse(str="", default_region="ZZ")
-    context.call "i18n.phonenumbers.PhoneNumberUtil.getInstance().parse", str, default_region
+    #context.call "i18n.phonenumbers.PhoneNumberUtil.getInstance().parse", str, default_region
+    context.call '(function (a,b) {
+        try{
+          var phoneUtil = i18n.phonenumbers.PhoneNumberUtil.getInstance();
+          var number = phoneUtil.parseAndKeepRawInput(a, b);
+          var VR = i18n.phonenumbers.PhoneNumberUtil.ValidationResult;
+          var _r = phoneUtil.isPossibleNumberWithReason(number);
+          var _is_reason = null;
+          for(reason in VR){
+            if (_r == VR[reason]){
+              _is_reason = reason;
+              break;
+            }
+          }
+          return {
+            util: phoneUtil.parse(a,b),
+            country: phoneUtil.getRegionCodeForNumber(number),
+            type: phoneUtil.getNumberType(number),
+            e164_number: phoneUtil.format(number, 0),
+            is_possible_number: phoneUtil.isPossibleNumber(number),
+            validation_reasult: _is_reason
+          };
+        }catch(e){
+          return {
+            message: e,
+            error: true
+          }
+        }
+      })
+    ', str, default_region
   end
   
   def simple
